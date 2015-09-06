32daa30fe1e1c82c4c0b1b90da7bf220860cf9b9
Rolf Krahl <rolf@rotkraut.de>
2015-09-06 14:46:59 +0000

    Calculate the md5 checksum while creating an index.

--- photo/idxitem.py
+++ photo/idxitem.py
@@ -1,6 +1,22 @@
 """Provide the class IdxItem which represents an item in the index.
 """
 
+import hashlib
+
+
+def _md5file(fname):
+    """Calculate the md5 hash for a file.
+    """
+    m = hashlib.md5()
+    chunksize = 8192
+    with open(fname, 'rb') as f:
+        while True:
+            chunk = f.read(chunksize)
+            if not chunk:
+                break
+            m.update(chunk)
+    return m.hexdigest()
+
 
 class IdxItem(object):
 
@@ -11,6 +27,7 @@ class IdxItem(object):
             self.__dict__.update(data)
         elif filename is not None:
             self.filename = filename
+            self.md5 = _md5file(filename)
 
     def as_dict(self):
         return dict(self.__dict__)
