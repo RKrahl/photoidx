b9568389d4dfa6f5c1696816d4f87d07922d3b0b
Rolf Krahl <rolf@rotkraut.de>
2016-04-03 19:29:25 +0200

    Add an entry to the TagSelectDialog to set new tags.  Close #10 and Fix #6.

--- photo/qt/tagSelectDialog.py
+++ photo/qt/tagSelectDialog.py
@@ -11,17 +11,16 @@ class TagSelectDialog(QtGui.QDialog):
     def __init__(self, taglist):
         super(TagSelectDialog, self).__init__()
 
-        self.taglist = set(taglist)
         self.checkLayout = QtGui.QGridLayout()
-        self.tagCheck = {}
-        ncol = int(math.ceil(math.sqrt(len(self.taglist)/10)))
-        nrow = int(math.ceil(len(self.taglist)/ncol))
-        c = 0
-        for t in sorted(self.taglist):
-            cb = QtGui.QCheckBox(t)
-            self.checkLayout.addWidget(cb, c % nrow, c // nrow)
-            self.tagCheck[t] = cb
-            c += 1
+        self.settags(taglist)
+
+        self.entry = QtGui.QLineEdit()
+        self.entry.returnPressed.connect(self.newtag)
+        entryLabel = QtGui.QLabel("New tag:")
+        entryLabel.setBuddy(self.entry)
+        entryLayout = QtGui.QHBoxLayout()
+        entryLayout.addWidget(entryLabel)
+        entryLayout.addWidget(self.entry)
 
         buttonBox = QtGui.QDialogButtonBox(QtGui.QDialogButtonBox.Ok | 
                                            QtGui.QDialogButtonBox.Cancel)
@@ -30,9 +29,47 @@ class TagSelectDialog(QtGui.QDialog):
 
         mainLayout = QtGui.QVBoxLayout()
         mainLayout.addLayout(self.checkLayout)
+        mainLayout.addLayout(entryLayout)
         mainLayout.addWidget(buttonBox, alignment=QtCore.Qt.AlignHCenter)
         self.setLayout(mainLayout)
         self.setWindowTitle("Select tags")
+
+    def settags(self, tags):
+        self.taglist = set(tags)
+        self.tagCheck = {}
+        # We need to rearrange the checkbox widgets in
+        # self.checkLayout.  To this end, remove all old checkboxes
+        # first, store those to be retained to self.tagCheck and then
+        # add the new set of checkboxes to self.checkLayout, reusing
+        # those in self.tagCheck.
+        while True:
+            child = self.checkLayout.takeAt(0)
+            if not child:
+                break
+            cb = child.widget()
+            t = cb.text()
+            if t in self.taglist:
+                self.tagCheck[t] = cb
+        if len(self.taglist) > 0:
+            ncol = int(math.ceil(math.sqrt(len(self.taglist)/10)))
+            nrow = int(math.ceil(len(self.taglist)/ncol))
+            c = 0
+            for t in sorted(self.taglist):
+                if t not in self.tagCheck:
+                    self.tagCheck[t] = QtGui.QCheckBox(t)
+                cb = self.tagCheck[t]
+                self.checkLayout.addWidget(cb, c % nrow, c // nrow)
+                c += 1
+
+    def newtag(self):
+        t = self.entry.text()
+        if t not in self.taglist:
+            self.taglist.add(t)
+            self.settags(self.taglist)
+        self.tagCheck[t].setCheckState(QtCore.Qt.Checked)
+        self.updateGeometry()
+        self.adjustSize()
+        self.entry.setText("")
 
     def setCheckedTags(self, tags):
         for t in self.taglist:
