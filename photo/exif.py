88046529000fc7aa7b606fc491ff272391e196ab
Rolf Krahl <rolf@rotkraut.de>
2016-04-09 07:12:01 +0200

    Move extraction of exif attributes in individual helper functions.

--- photo/exif.py
+++ photo/exif.py
@@ -19,22 +19,32 @@ class Exif(object):
 
     def __init__(self, filename):
         self._exif = GExiv2.Metadata(filename)
+        self.createdate = self._get_date_time()
+        self.orientation = self._get_orientation()
+        self.gpsPosition = self._get_gpsPosition()
+
+    def _get_date_time(self):
         try:
-            self.createdate = self._exif.get_date_time()
+            dt = self._exif.get_date_time()
         except KeyError:
-            self.createdate = None
+            return None
+        return dt
+
+    def _get_orientation(self):
         try:
             orientation = self._exif.get_orientation()
-            self.orientation = self.OrientationXlate[int(orientation)]
+            return self.OrientationXlate[int(orientation)]
         except KeyError:
-            self.orientation = None
+            return None
+
+    def _get_gpsPosition(self):
         if ('Exif.GPSInfo.GPSLatitude' in self._exif and
             'Exif.GPSInfo.GPSLongitude' in self._exif):
             lat = self._exif.get_gps_latitude()
             lon = self._exif.get_gps_longitude()
             latref = 'N' if lat >= 0.0 else 'S'
             lonref = 'E' if lon >= 0.0 else 'W'
-            self.gpsPosition = { latref:abs(lat), lonref:abs(lon) }
+            return { latref:abs(lat), lonref:abs(lon) }
         else:
-            self.gpsPosition = None
+            return None
 
