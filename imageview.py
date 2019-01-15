5e2cb0489d49deb92b294b45b61730a4b9135845
Rolf Krahl <rolf@rotkraut.de>
2019-01-15 08:59:51 +0100

    Add --readOnly and --create command line flags to imageview.py.

--- imageview.py
+++ imageview.py
@@ -11,16 +11,28 @@ from photo.qt import ImageViewer
 argparser = argparse.ArgumentParser()
 argparser.add_argument('-d', '--directory', help="image directory", default=".")
 argparser.add_argument('--scale', help="scale factor")
+argparser.add_argument('--readOnly', 
+                       help='access the index in read only mode', 
+                       action='store_const', const=True)
+argparser.add_argument('--create', 
+                       help='create the index if not present', 
+                       action='store_const', const=True)
 photo.idxfilter.addFilterArguments(argparser)
 args = argparser.parse_args()
 
 app = QtGui.QApplication([])
 try:
     idx = photo.index.Index(idxfile=args.directory)
-    readOnly=False
+    readOnly = args.readOnly
+    dirty = False
 except OSError:
     idx = photo.index.Index(imgdir=args.directory)
-    readOnly=True
+    if args.create:
+        readOnly = args.readOnly
+        dirty = True
+    else:
+        readOnly = True
+        dirty = False
 idxfilter = photo.idxfilter.IdxFilter.from_args(args)
-imageViewer = ImageViewer(idx, idxfilter, args.scale, readOnly)
+imageViewer = ImageViewer(idx, idxfilter, args.scale, readOnly, dirty)
 sys.exit(app.exec_())
