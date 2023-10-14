d8b33b070218f2ae121555d314dffc96819dfc20
Rolf Krahl <rolf@rotkraut.de>
2023-10-14 17:44:23 +0200

    Update copyright date

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -12,7 +12,7 @@ import _meta
 # -- Project information -----------------------------------------------------
 
 project = 'photoidx'
-copyright = '2015–2022, Rolf Krahl'
+copyright = '2015–2023, Rolf Krahl'
 author = 'Rolf Krahl'
 
 # The full version, including alpha/beta/rc tags
