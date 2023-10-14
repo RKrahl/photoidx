3fa6ef372cf71750ff0b1d9d385cb8449a3c93a3
Rolf Krahl <rolf@rotkraut.de>
2023-10-14 15:19:12 +0200

    Add tests for GeoPosition.__sub__() and GeoPosition.centroid()

--- tests/test_01_geo.py
+++ tests/test_01_geo.py
@@ -28,6 +28,10 @@ pos_berlin = PositionData(52.51959, 13.40684, "berlin")
 pos_philly = PositionData(39.94918, -75.14993, "philly")
 pos_vfalls = PositionData(-17.92304, 25.84708, "vic_falls")
 pos_sirius = PositionData(-22.80778, -47.05250, "sirius")
+# and we want a couple of points that are close to each other
+pos_spree1 = PositionData(51.00971, 14.64958, "spree1")
+pos_spree2 = PositionData(50.98313, 14.61911, "spree2")
+pos_spree3 = PositionData(50.98752, 14.60632, "spree3")
 
 
 @pytest.mark.parametrize("pos", [
@@ -76,3 +80,54 @@ def test_geo_str(pos):
     # new GeoPosition instance
     p1 = GeoPosition(p.floatstr())
     assert (p1.lat, p1.lon) == pytest.approx((p.lat, p.lon))
+
+@pytest.mark.parametrize("pos1, pos2", [
+    pytest.param(pos_berlin, pos_vfalls,
+                 id="%s-%s" % (pos_berlin.name, pos_vfalls.name)),
+    pytest.param(pos_vfalls, pos_philly,
+                 id="%s-%s" % (pos_vfalls.name, pos_philly.name)),
+    pytest.param(pos_philly, pos_berlin,
+                 id="%s-%s" % (pos_philly.name, pos_berlin.name)),
+    pytest.param(pos_berlin, pos_spree1,
+                 id="%s-%s" % (pos_berlin.name, pos_spree1.name)),
+    pytest.param(pos_spree1, pos_spree2,
+                 id="%s-%s" % (pos_spree1.name, pos_spree2.name)),
+    pytest.param(pos_spree2, pos_spree3,
+                 id="%s-%s" % (pos_spree2.name, pos_spree3.name)),
+])
+def test_geo_center_dist(pos1, pos2):
+    """Simple test for distance and centroid for GeoPosition objects.
+
+    The centroid of two points should be the point having half the
+    distance to either point.
+    """
+    p1 = GeoPosition((pos1.lat, pos1.lon))
+    p2 = GeoPosition((pos2.lat, pos2.lon))
+    d = (p1 - p2) / 2
+    c = GeoPosition.centroid((p1, p2))
+    assert (p1 - c) == pytest.approx(d)
+    assert (p2 - c) == pytest.approx(d)
+
+@pytest.mark.parametrize("pos1, pos2, pos3", [
+    pytest.param(pos_spree1, pos_spree2, pos_spree3,
+                 id=("%s-%s-%s"
+                     % (pos_spree1.name, pos_spree2.name, pos_spree3.name))),
+])
+def test_geo_triangle_centroid(pos1, pos2, pos3):
+    """A complex triangle centroid test.
+
+    Consider a triangle and a second triangle defined by the centroids
+    of the three edges of the first triangle.  Both triangles should
+    have the same centroid.  This test only works for positions that
+    are not to far from each other, otherwise the inaccuracies from
+    projecting the centroids to the earth's surface become to large.
+    """
+    p1 = GeoPosition((pos1.lat, pos1.lon))
+    p2 = GeoPosition((pos2.lat, pos2.lon))
+    p3 = GeoPosition((pos3.lat, pos3.lon))
+    c0 = GeoPosition.centroid((p1, p2, p3))
+    e1 = GeoPosition.centroid((p2, p3))
+    e2 = GeoPosition.centroid((p1, p3))
+    e3 = GeoPosition.centroid((p1, p2))
+    c1 = GeoPosition.centroid((e1, e2, e3))
+    assert (c1.lat, c1.lon) == pytest.approx((c0.lat, c0.lon))
