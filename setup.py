e3f7d1410d2a1087b91042772589e227b9b7c6b9
Rolf Krahl <rolf@rotkraut.de>
2024-12-01 14:53:59 +0100

    Copy _meta.py to build_lib on setup build

--- setup.py
+++ setup.py
@@ -97,6 +97,9 @@ class build_py(setuptools.command.build_py.build_py):
     def run(self):
         self.run_command('meta')
         super().run()
+        package = self.distribution.packages[0].split('.')
+        outfile = self.get_module_outfile(self.build_lib, package, "_meta")
+        self.copy_file("_meta.py", outfile, preserve_mode=0)
 
 
 with Path("README.rst").open("rt", encoding="utf8") as f:
