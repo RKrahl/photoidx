52cfa8d4fc95757b94dc54ac9f6017b0e688115c
Rolf Krahl <rolf@rotkraut.de>
2024-12-01 14:41:50 +0100

    Rename the '__version__' attribute in _meta.py to 'version'

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ try:
 except (ImportError, LookupError):
     try:
         import _meta
-        version = _meta.__version__
+        version = _meta.version
     except ImportError:
         log.warn("warning: cannot determine version number")
         version = "UNKNOWN"
@@ -41,7 +41,7 @@ class meta(setuptools.Command):
 __version__ = "%(version)s"
 '''
     meta_template = '''
-__version__ = "%(version)s"
+version = "%(version)s"
 '''
 
     def initialize_options(self):
